3f7c5f6d58c36bc69519570259be6c8f2c07dabe
huang-qiruiqq <3208137151@qq.com>
2026-01-05 16:10:08 +0800

    QWidget 的cursor属性

--- code/QWidget_8/widget.cpp
+++ code/QWidget_8/widget.cpp
@@ -7,8 +7,13 @@ Widget::Widget(QWidget *parent)
 {
     ui->setupUi(this);
 
-    QCursor cursor(Qt::OpenHandCursor);
-    ui->pushButton->setCursor(cursor);
+//    QCursor cursor(Qt::OpenHandCursor);
+//    ui->pushButton->setCursor(cursor);
+    // 访问一个图片
+    QPixmap pixmap(":/code.png");
+    // pixmap = pixmap.scaled(100, 100); // 缩放图片
+    QCursor cursor(pixmap, 10, 10); // 默认热点位置会是图片左上角
+    this->setCursor(cursor);
 }
 
 Widget::~Widget()
