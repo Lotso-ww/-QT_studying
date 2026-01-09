ea886575af015de1f39ed5f35ccdb12b1eaf8d75
huang-qiruiqq <3208137151@qq.com>
2026-01-09 14:33:19 +0800

    QLineEdit的使用和属性

---
+++ code/QLineEdit_4/widget.cpp
@@ -0,0 +1,17 @@
+#include "widget.h"
+#include "ui_widget.h"
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+
+    ui->lineEdit->setEchoMode(QLineEdit::Password);
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
