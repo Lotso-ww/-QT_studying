7b2f9f4ca3d63fe0b835875e6c2b519d390b9671
huang-qiruiqq <3208137151@qq.com>
2026-01-13 15:52:41 +0800

    QVBoxLayout的属性和使用

--- code/QVBoxLayout/widget.cpp
+++ code/QVBoxLayout/widget.cpp
@@ -9,7 +9,7 @@ Widget::Widget(QWidget *parent)
 {
     ui->setupUi(this);
 
-    // 创建三个三年，使用垂直布局管理器管理起来
+    // 创建三个按钮，使用垂直布局管理器管理起来
     QPushButton* button1 = new QPushButton("按钮1");
     QPushButton* button2 = new QPushButton("按钮2");
     QPushButton* button3 = new QPushButton("按钮3");
