24ea13ef795ac472c76e52c07ede562c87d2e952
huang-qiruiqq <3208137151@qq.com>
2026-02-08 15:56:52 +0800

    给控件设置样式

--- code/build-qss_13-Desktop_Qt_5_14_2_MinGW_64_bit-Debug/ui_widget.h
+++ code/build-qss_13-Desktop_Qt_5_14_2_MinGW_64_bit-Debug/ui_widget.h
@@ -20,6 +20,7 @@ class Ui_Widget
 {
 public:
     QListWidget *listWidget;
+    QListWidget *listWidget_2;
 
     void setupUi(QWidget *Widget)
     {
@@ -38,6 +39,46 @@ public:
 "\n"
 "QListWidget::item:selected{\n"
 "	background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1, stop: 0 #6a6ea9, stop: 1 #888dd9);\n"
+"}"));
+        listWidget_2 = new QListWidget(Widget);
+        new QListWidgetItem(listWidget_2);
+        new QListWidgetItem(listWidget_2);
+        new QListWidgetItem(listWidget_2);
+        new QListWidgetItem(listWidget_2);
+        new QListWidgetItem(listWidget_2);
+        listWidget_2->setObjectName(QString::fromUtf8("listWidget_2"));
+        listWidget_2->setGeometry(QRect(150, 390, 256, 192));
+        listWidget_2->setStyleSheet(QString::fromUtf8("/* \345\210\227\350\241\250\351\241\271\351\273\230\350\256\244\346\240\267\345\274\217 */\n"
+"QListView::item {\n"
+"    height: 30px; /* \345\210\227\350\241\250\351\241\271\351\253\230\345\272\246 */\n"
+"    padding-left: 10px; /* \346\226\207\345\255\227\345\267\246\351\227\264\350\267\235 */\n"
+"}\n"
+"\n"
+"/* \351\274\240\346\240\207\346\202\254\345\201\234\357\274\232\345\236\202\347\233\264\346\270\220\345\217\230\357\274\210\346\265\205\350\223\235\342\206\222\346\265\205\347\264\253\357\274\211 */\n"
+"QListView::item:hover {\n"
+"    background: qlineargradient(\n"
+"        x1: 0, y1: 0, x2: 0, y2: 1,\n"
+"        stop: 0 #FAFBFE,  /* \351\241\266\351\203\250\351\242\234\350\211\262 */\n"
+"        stop: 1 #DCDEF1   /* \345\272\225\351\203\250\351\242\234\350\211\262 */\n"
+"    );\n"
+"}\n"
+"\n"
+"/* \351\200\211\344\270\255\347\212\266\346\200\201\357\274\232\345\236\202\347\233\264\346\270\220\345\217\230\357\274\210\346\267\261\350\223\235\342\206\222\346\267\261\347\264\253\357\274\211+ \350\276\271\346"
+                        "\241\206 */\n"
+"QListView::item:selected {\n"
+"    border: 1px solid #6a6ea9; /* \351\200\211\344\270\255\350\276\271\346\241\206 */\n"
+"    background: qlineargradient(\n"
+"        x1: 0, y1: 0, x2: 0, y2: 1,\n"
+"        stop: 0 #6a6ea9,  /* \351\241\266\351\203\250\351\242\234\350\211\262 */\n"
+"        stop: 1 #888dd9   /* \345\272\225\351\203\250\351\242\234\350\211\262 */\n"
+"    );\n"
+"    color: #fff; /* \351\200\211\344\270\255\346\226\207\345\255\227\351\242\234\350\211\262 */\n"
+"}\n"
+"\n"
+"/* \345\210\227\350\241\250\346\241\206\350\203\214\346\231\257\357\274\210\345\216\273\351\231\244\351\273\230\350\256\244\347\231\275\350\211\262\357\274\211 */\n"
+"QListView {\n"
+"    background-color: #f8f8f8;\n"
+"    border: none; /* \345\217\226\346\266\210\351\273\230\350\256\244\350\276\271\346\241\206 */\n"
 "}"));
 
         retranslateUi(Widget);
@@ -58,6 +99,21 @@ public:
         QListWidgetItem *___qlistwidgetitem2 = listWidget->item(2);
         ___qlistwidgetitem2->setText(QCoreApplication::translate("Widget", "333", nullptr));
         listWidget->setSortingEnabled(__sortingEnabled);
+
+
+        const bool __sortingEnabled1 = listWidget_2->isSortingEnabled();
+        listWidget_2->setSortingEnabled(false);
+        QListWidgetItem *___qlistwidgetitem3 = listWidget_2->item(0);
+        ___qlistwidgetitem3->setText(QCoreApplication::translate("Widget", "111", nullptr));
+        QListWidgetItem *___qlistwidgetitem4 = listWidget_2->item(1);
+        ___qlistwidgetitem4->setText(QCoreApplication::translate("Widget", "222", nullptr));
+        QListWidgetItem *___qlistwidgetitem5 = listWidget_2->item(2);
+        ___qlistwidgetitem5->setText(QCoreApplication::translate("Widget", "333", nullptr));
+        QListWidgetItem *___qlistwidgetitem6 = listWidget_2->item(3);
+        ___qlistwidgetitem6->setText(QCoreApplication::translate("Widget", "444", nullptr));
+        QListWidgetItem *___qlistwidgetitem7 = listWidget_2->item(4);
+        ___qlistwidgetitem7->setText(QCoreApplication::translate("Widget", "555", nullptr));
+        listWidget_2->setSortingEnabled(__sortingEnabled1);
 
     } // retranslateUi
 
--- code/build-qss_14-Desktop_Qt_5_14_2_MinGW_64_bit-Debug/ui_mainwindow.h
+++ code/build-qss_14-Desktop_Qt_5_14_2_MinGW_64_bit-Debug/ui_mainwindow.h
@@ -39,7 +39,7 @@ public:
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
         MainWindow->resize(800, 600);
-        MainWindow->setStyleSheet(QString::fromUtf8("QMenuBar{\n"
+        MainWindow->setStyleSheet(QString::fromUtf8("/*QMenuBar{\n"
 "	background-color: rgb(170, 170, 255);\n"
 "	spacing: 5px;\n"
 "}\n"
@@ -67,6 +67,63 @@ public:
 "	heighht: 2px;\n"
 "	background-color: green;\n"
 "	margin: 0 5px;\n"
+"}*/\n"
+"\n"
+"/* \350\217\234\345\215\225\346\240\217\350\203\214\346\231\257\357\274\232\345\236\202\347\233\264\346\270\220\345\217\230\357\274\210\346\265\205\347\201\260\342\206\222\346\267\261\347\201\260\357\274\211 */\n"
+"QMenuBar {\n"
+"    background-color: qlineargradient(\n"
+"        x1: 0, y1: 0, x2: 0, y2: 1,\n"
+"        stop: 0 lightgray,\n"
+"        stop: 1 darkgray\n"
+"    );\n"
+"    spacing: 3px; /* \350\217\234\345\215\225\351\241\271\351\227\264\350\267\235 */\n"
+"}\n"
+""
+                        "\n"
+"/* \350\217\234\345\215\225\351\241\271\351\273\230\350\256\244\346\240\267\345\274\217 */\n"
+"QMenuBar::item {\n"
+"    padding: 1px 4px; /* \345\206\205\350\276\271\350\267\235 */\n"
+"    background: transparent; /* \351\200\217\346\230\216\350\203\214\346\231\257 */\n"
+"    border-radius: 4px; /* \345\234\206\350\247\222 */\n"
+"}\n"
+"\n"
+"/* \350\217\234\345\215\225\351\241\271\351\200\211\344\270\255\357\274\210\351\274\240\346\240\207\346\202\254\346\265\256/\351\200\211\344\270\255\357\274\211 */\n"
+"QMenuBar::item:selected {\n"
+"    background-color: #a8a8a8;\n"
+"}\n"
+"\n"
+"/* \350\217\234\345\215\225\351\241\271\346\214\211\345\216\213 */\n"
+"QMenuBar::item:pressed {\n"
+"    background-color: #888888;\n"
+"}\n"
+"\n"
+"/* \344\270\213\346\213\211\350\217\234\345\215\225\350\203\214\346\231\257+\351\227\264\350\267\235 */\n"
+"QMenu {\n"
+"    background-color: white;\n"
+"    margin: 0 2px; /* \350\217\234\345\215\225\350\276\271\347\274\230\351\227\264\350\267\235 */\n"
+"    border: 1px solid #ddd; /* \350"
+                        "\217\234\345\215\225\350\276\271\346\241\206 */\n"
+"}\n"
+"\n"
+"/* \344\270\213\346\213\211\350\217\234\345\215\225\351\241\271\346\240\267\345\274\217 */\n"
+"QMenu::item {\n"
+"    padding: 2px 25px 2px 20px; /* \345\206\205\350\276\271\350\267\235\357\274\232\345\267\246\345\217\263\351\242\204\347\225\231\347\251\272\351\227\264 */\n"
+"    border: 3px solid transparent; /* \351\200\211\344\270\255\350\276\271\346\241\206\351\242\204\347\225\231\347\251\272\351\227\264 */\n"
+"}\n"
+"\n"
+"/* \344\270\213\346\213\211\350\217\234\345\215\225\351\241\271\351\200\211\344\270\255 */\n"
+"QMenu::item:selected {\n"
+"    border-color: darkblue; /* \351\200\211\344\270\255\350\276\271\346\241\206 */\n"
+"    background-color: rgba(100, 100, 100, 0.1); /* \345\215\212\351\200\217\346\230\216\350\203\214\346\231\257 */\n"
+"}\n"
+"\n"
+"/* \350\217\234\345\215\225\345\210\206\351\232\224\347\272\277 */\n"
+"QMenu::separator {\n"
+"    height: 2px; /* \345\210\206\351\232\224\347\272\277\351\253\230\345\272\246 */\n"
+"    backgrou"
+                        "nd-color: lightblue; /* \345\210\206\351\232\224\347\272\277\351\242\234\350\211\262 */\n"
+"    margin-left: 10px; /* \345\267\246\351\227\264\350\267\235 */\n"
+"    margin-right: 5px; /* \345\217\263\351\227\264\350\267\235 */\n"
 "}"));
         action111 = new QAction(MainWindow);
         action111->setObjectName(QString::fromUtf8("action111"));
@@ -81,7 +138,7 @@ public:
         MainWindow->setCentralWidget(centralwidget);
         menubar = new QMenuBar(MainWindow);
         menubar->setObjectName(QString::fromUtf8("menubar"));
-        menubar->setGeometry(QRect(0, 0, 800, 22));
+        menubar->setGeometry(QRect(0, 0, 800, 18));
         menu_2 = new QMenu(menubar);
         menu_2->setObjectName(QString::fromUtf8("menu_2"));
         menu_3 = new QMenu(menubar);
